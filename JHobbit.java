5a58824a097c3944860031ea5f07c1231c44a989
Aeyzaz <aeyzaz@hotmail.com>
2014-07-02 19:18:07 +0500

    JHobbit

--- JHobbit.java
+++ JHobbit.java
@@ -178,6 +178,27 @@ public class JHobbit{
    return passwordok;
    }
    
+    static int FindMaxInArray(int[] arr) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    
+    static int FindMinInArray(int[] arr) {
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
    
     
 }
